3089c3a2693aa18eb475a6402d2985a459266741
HuaCM <njbxhzy@hotmail.com>
2019-10-30 10:36:24 +0800

    change the file name

--- parse_BioProject.py
+++ parse_BioProject.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Time : 2019/9/30 11:03
 # @Author : Zhongyi Hua
-# @FileName: Parse_BioProject.py
+# @FileName: parse_BioProject.py
 # @Usage: 
 # @Note:
 # @E-mail: njbxhzy@hotmail.com
