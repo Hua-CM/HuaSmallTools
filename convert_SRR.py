c2f4bcfe8975127071ebd31dc5bd3a676b9141b4
Hua-CM <njbxhzy@hotmail.com>
2020-03-09 17:43:40 +0800

    add fastq download option

--- convert_SRR.py
+++ convert_SRR.py
@@ -31,12 +31,11 @@ def convert_srr(input_file, output_file):
 def convert_fastq(input_file, output_file):
     with open(input_file, "r") as num_list:
         for sra_number in num_list:
-            srr_list = get_html(f"https://www.ebi.ac.uk/ena/data/warehouse/filereport?accession\
-                                  ={sra_number}&result=read_run&fields=fastq_ftp")
+            srr_list = get_html(f"https://www.ebi.ac.uk/ena/data/warehouse/filereport?accession={sra_number.strip()}&result=read_run&fields=fastq_ftp")
             srr_list = list(chain.from_iterable([x.split(";") for x in srr_list.split("\n")[1:]]))
             srr_list = [re.sub("ftp\\.sra\\.ebi\\.ac\\.uk", "era-fasp@fasp.sra.ebi.ac.uk:", x) for x in srr_list]
             with open(output_file, "a") as f:
-                f.writelines(sra_number + "\n" + "\n".join(srr_list))
+                f.writelines(sra_number + "\n".join(srr_list[:-1]) + "\n")
 
 
 if __name__ == '__main__':
