cfb9a931f19a4c31b0788dbb7dbe287dbadf89eb
HuaCM <njbxhzy@hotmail.com>
2020-08-30 22:05:35 +0800

    add DDBJ links

--- convert/convert_aspera.py
+++ convert/convert_aspera.py
@@ -19,13 +19,15 @@ def get_html(url):
 
 def convert_ncbi(input_file, output_file):
     with open(input_file, "r") as num_list:
-        a = re.compile(r"[DES]RR(\d{6,8})")
+        srr_par = re.compile(r"[DES]RR(\d{6,8})")
         for line in num_list:
             tmp_text = get_html("https://www.ncbi.nlm.nih.gov/sra/?term=" + line.strip())
-            b = re.search(a, tmp_text).group()
-            c = "/".join(["anonftp@ftp.ncbi.nlm.nih.gov:/sra/sra-instant/reads/ByRun/sra", b[0:3], b[0:6], b, b+".sra"])
+            srr_acc = re.search(srr_par, tmp_text).group()
+            ascp_link = "/".join(["anonftp@ftp.ncbi.nlm.nih.gov:/sra/sra-instant/reads/ByRun/sra", srr_acc[0:3],
+                          srr_acc[0:6], srr_acc,
+                          srr_acc+".sra"])
             with open(output_file, "a") as f:
-                f.writelines(line.strip()+"\t"+b+"\t"+c+"\n")
+                f.writelines(line.strip()+"\t"+srr_acc+"\t"+ascp_link+"\n")
 
 
 def convert_ena(input_file, output_file):
@@ -35,18 +37,21 @@ def convert_ena(input_file, output_file):
             srr_list = list(chain.from_iterable([x.split('\t')[1].split(";") for x in srr_list.split("\n")[1:-1]]))
             srr_list = [re.sub("ftp\\.sra\\.ebi\\.ac\\.uk", "era-fasp@fasp.sra.ebi.ac.uk:", x) for x in srr_list]
             with open(output_file, "a") as f:
-                f.writelines("\n".join([sra_number.strip() + "\t" + x for x in srr_list[:-1]]) + "\n")
+                f.writelines("\n".join([sra_number.strip() + "\t" + x for x in srr_list]) + "\n")
 
 
 def convert_ddbj(input_file, output_file):
     with open(input_file, "r") as num_list:
-        a = re.compile(r"[DES]RR(\d{6,8})")
+        srr_par = re.compile(r"[DES]RR(\d{6,8})")
+        srx_par = re.compile(r"[DES]RX(\d{6,8})")
         for line in num_list:
-            tmp_text = requests.post('https://ddbj.nig.ac.jp/DRASearch/', {'acc': line.strip()}, headers=headers).text
-            b = re.search(a, tmp_text).group()
-            c = "/".join(['anonftp@ascp.ddbj.nig.ac.jp:/ddbj_database/dra/fastq', b[0:3], b[0:6], b, b+'.sra'])
+            tmp_text = get_html(r'https://ddbj.nig.ac.jp/DRASearch/query?acc=' + line.strip() + '&show=20&sort=Study')
+            srr_acc = re.search(srr_par, tmp_text).group()
+            srx_acc = re.search(srx_par, tmp_text).group()
+            c = "/".join(['anonftp@ascp.ddbj.nig.ac.jp:/ddbj_database/dra/sralite/ByExp/litesra', srx_acc[0:3],
+                          srx_acc[0:6], srx_acc, srr_acc, srr_acc+'.sra'])
             with open(output_file, 'a') as f:
-                f.writelines(line.strip()+'\t'+b+'\t'+c+'\n')
+                f.writelines(line.strip()+'\t'+srr_acc+'\t'+c+'\n')
 
 
 if __name__ == '__main__':
