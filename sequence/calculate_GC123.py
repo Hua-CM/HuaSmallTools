e207836a3e7b03c27b693bd869d599a7e5805579
HuaCM <njbxhzy@hotmail.com>
2021-03-07 20:51:31 +0800

    polish

--- sequence/calculate_GC123.py
+++ sequence/calculate_GC123.py
@@ -11,19 +11,24 @@ from Bio import SeqIO
 import pandas as pd
 from sequence.calculate_motif import motif123
 import argparse
+from os.path import split as splt
 
 
 def calculate123(seq_path):
-    result_df = pd.DataFrame(columns=["seqid", "GC", "GC12", "GC1", "GC2", "GC3"])
     seq_list = SeqIO.parse(seq_path, "fasta")
+    dict_list = []
     for record in seq_list:
         tmp_dict = dict()
-        tmp_dict["seqid"] = record.id
-        tmp_dict["GC"], tmp_dict["GC1"], tmp_dict["GC2"], tmp_dict["GC3"] = GC123(record.seq)
-        tmp_dict["GC12"] = (tmp_dict["GC1"] + tmp_dict["GC2"])/2
-        result_df = result_df.append(tmp_dict, sort=False, ignore_index=True)
-    result_df.iloc[:, 1:] = result_df.iloc[:, 1:]/100
-    return result_df
+        tmp_dict["GC"], tmp_dict["GC1"], tmp_dict["GC2"], tmp_dict["GC3"] = GC123(record.seq)/100
+        tmp_dict['slen'] = len(record.seq)
+        dict_list.append(tmp_dict)
+    result_df = pd.DataFrame(dict_list)
+    result_dict = {'seqid': splt(seq_path)[1],
+                   'GC': sum(result_df.GC * result_df.slen) / result_df.slen.sum()/100,
+                   'GC1': sum(result_df.GC1 * result_df.slen) / result_df.slen.sum()/100,
+                   'GC2': sum(result_df.GC2 * result_df.slen) / result_df.slen.sum()/100,
+                   'GC3': sum(result_df.GC3 * result_df.slen) / result_df.slen.sum()/100}
+    return result_dict
 
 
 def calculateATCG3(seq_path):
@@ -41,17 +46,10 @@ def calculateATCG3(seq_path):
     return result_df
 
 
-def calculate_codon_analysis(seq_path):
-    gc_df = calculate123(seq_path)
-    atcg_df = calculateATCG3(seq_path)
-    gc_df = pd.merge(gc_df, atcg_df, on="seqid")
-    return gc_df
-
-
 def parse_args():
     parser = argparse.ArgumentParser(description="This is the script for calculate GC123")
     parser.add_argument('-i', '--input_file', required=True,
-                        help='<file path>  The cds sequence in fasta format')
+                        help='<file path>  A file containing the path of the cds sequence in fasta format')
     parser.add_argument('-a', '--ATCG', default=False, action='store_true',
                         help='calculate A3, T3, G3, C3 as well')
     parser.add_argument('-t', '--out_file', required=True,
@@ -61,17 +59,20 @@ def parse_args():
 
 
 def main(args):
-    result_df = []
+    result_list = []
     with open(args.input_file) as f:
         file_list = [_.strip() for _ in f.read().split('\n')]
     for file in file_list:
-        result_df.append(calculate123(file))
-    GC_DF = pd.concat(result_df)
+        try:
+            result_list.append(calculate123(file))
+        except:
+            continue
+    GC_DF = pd.DataFrame(result_list)
     if args.ATCG:
-        result_df2 = []
+        result_list2 = []
         for file in file_list:
-            result_df.append(calculateATCG3(file))
-        GC_DF2 = pd.concat(result_df2)
+            result_list2.append(calculateATCG3(file))
+        GC_DF2 = pd.DataFrame(result_list2)
         GC_DF = pd.merge(GC_DF, GC_DF2, on='seqid')
     GC_DF.to_csv(args.out_file, sep="\t", index=False)
 
