cfcff3147fc3672e3dcf6863eebb1d5b844f998a
HuaCM <njbxhzy@hotmail.com>
2021-03-12 13:14:04 +0800

    add chinese to latin function

--- spider/spider_chinese_name.py
+++ spider/spider_chinese_name.py
@@ -16,7 +16,7 @@ from time import sleep
 
 def get_html(url):
     my_header = {
-        'User-Agent': r"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0"}
+        'User-Agent': r'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0'}
     return requests.get(url, headers=my_header).text
 
 
@@ -24,24 +24,20 @@ def get_chinese_name(latin_name, retry_num=0):
     retry_num += 1
     if retry_num > 3:
         return
-    query_url = "http://www.iplant.cn/info/" + latin_name
+    query_url = 'http://www.iplant.cn/info/' + latin_name
     try:
         a = Pq(get_html(query_url))
         try:
-            chinese_name_inner = re.match("[\u4e00-\u9fa5]+", a('head>title').text()).group(0)
+            chinese_name_inner = re.match('[\u4e00-\u9fa5]+', a('head>title').text()).group(0)
             if chinese_name_inner == "植物智":
                 return
             return chinese_name_inner
         except AttributeError:
-            synom_name = a('.infomore>a').text()
+            synom_name = a('.infomore .spantxt').text()
             if not synom_name == '':
-                query_url = "http://www.iplant.cn/info/" + synom_name
-                a = Pq(get_html(query_url))
-                try:
-                    chinese_name_inner = re.match("[\u4e00-\u9fa5]+", a('head>title').text()).group(0)
-                    return chinese_name_inner + "\t" + synom_name
-                except AttributeError:
-                    return None
+                return re.search('[\u4e00-\u9fa5]+', synom_name).group(0) + \
+                       '\t' + \
+                       re.search('[a-zA-Z\s]+', synom_name).group(0).strip()
             else:
                 return
     except TimeoutError:
@@ -49,11 +45,35 @@ def get_chinese_name(latin_name, retry_num=0):
         get_chinese_name(latin_name, retry_num=retry_num)
 
 
+def get_latin_name(chinese_name, retry_num=0):
+    retry_num += 1
+    if retry_num > 3:
+        return
+    query_url = 'http://www.iplant.cn/info/' + chinese_name
+    try:
+        a = Pq(get_html(query_url))
+        latin_name = a('#sptitlel.infolatin').text()
+        if latin_name == '':
+            try:
+                return re.search('[a-zA-Z\s]+', a('.infomore>a').text()).group(0).strip() + \
+                       '\t' + \
+                       re.search('[\u4e00-\u9fa5]+', a('.infomore>a').text()).group(0)
+            except AttributeError:
+                return
+        return latin_name
+    except TimeoutError:
+        sleep(1)
+        get_chinese_name(chinese_name, retry_num=retry_num)
+
+
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="This is the script for spider Chinese name from iplants website.")
+        description='This is the script for spider Chinese name from iplants website.')
     parser.add_argument('-i', '--file_path', required=True,
                         help='<file_path>  A txt file contains one latin name per line')
+    parser.add_argument('-m', '--model', default='latin', choices=['latin', 'chinese'],
+                        help='<latin|chinese> "latin" for latin to chinese, '
+                             '"chinese" for chinese to latin. DEFAULT:latin')
     parser.add_argument('-o', '--result_path', required=True,
                         help='<file_path>  The result file')
     args = parser.parse_args()
@@ -61,14 +81,15 @@ def parse_args():
 
 
 def main(args):
+    func = get_chinese_name if args.model == 'latin' else get_latin_name
     result_list = []
-    with open(args.file_path, "r") as query_file:
-        query_list = query_file.read().split('n')
-        for query_name in tqdm(query_list):
-            chinese_name = get_chinese_name(query_name)
-            result_list.append(f"{query_name}\t{chinese_name}")
-    with open(args.result_path, "w") as result_file:
-        result_file.write("\n".join(result_list))
+    with open(args.file_path, 'r', encoding='utf-8') as query_file:
+        query_list = query_file.read().split('\n')
+    for query_name in tqdm(query_list):
+        chinese_name = func(query_name)
+        result_list.append(f'{query_name}\t{chinese_name}')
+    with open(args.result_path, 'w', encoding='utf-8') as result_file:
+        result_file.write('\n'.join(result_list))
         result_file.close()
 
 
